ff070603c3993abe6654f9573bbebac4cd8c2956
snowyxxdev <snowyware@gmail.com>
2024-10-06 11:05:00 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,6 @@
 """ Server Configuration (Ignore)"""
 import os;os.system("cls" if os.name == "nt" else "clear");os.system("title Console");
-
-
-""" _______________________________________________________________________________ """
-
-""" Main """
+# _____________________________________________________________________
 
 print("Hello, there! You've successfully connected to SnowyCloud servers.")
 name = input("Name > ")
