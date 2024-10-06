4b1c6cf029b3a3a8089bf5c3f94a0a687adecba5
snowyxxdev <snowyware@gmail.com>
2024-10-06 12:09:35 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 """ Server Configuration (Ignore)"""
 import os;os.system("cls" if os.name == "nt" else "clear");os.system("title Console");
+# Use exit() to exit out of the program since it won't exit at all without this function.
 # _____________________________________________________________________
+""" main """
+
 
 print("Hello, there! You've successfully connected to SnowyCloud servers.")
 name = input("Name > ")
 print(f"Nice to meet you, {name}.")
+exit()
